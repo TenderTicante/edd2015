1934946580eb7ee01e99ce292a469c86a42ab69d
Tender Ticante <tenderticante@gmail.com>
2015-10-12 09:52:07 -0500

    Arreglos para la segunda matriz dinamica

--- matrices/nxn.c
+++ matrices/nxn.c
@@ -1,37 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-	int renglones;
-        int columnas;
-	int **matriz;
-	int i;
+int main() {
+    int renglones;
+    int columnas;
+    int **matriz;
+    int i;
+    int ii;
+    int **matriz2;
+    int renglones2;
+    int columnas2;
+    
+    //Primera Matriz
+    printf("Numero de renglones: ");
+    scanf("%d", &renglones);
+    
+    printf("Numero de columnas: ");
+    scanf("%d", &columnas);
+    
+    matriz=(int**)malloc(sizeof(int*)*renglones);
+    
+    for (i=1; i<=renglones;i++){
+        matriz[i]=(int*)malloc(sizeof(int)*columnas);
+    }
 
-	printf("Numero de renglones: ");
-	scanf("%d", &renglones);
-        
-        printf("Numero de columnas: ");
-	scanf("%d", &columnas);
+        for(i=1;i<=renglones;i++){
+            for(ii=1;ii<=columnas;ii++){
+                 printf("Introducir valor de [%d][%d]: ",i,ii);
+                scanf("%d",&matriz[i][ii]);
+            }
+        }
+    
+    //Segunda Matriz
+    printf("Numero de renglones: ");
+    scanf("%d", &renglones2);
+    
+    printf("Numero de columnas: ");
+    scanf("%d", &columnas2);
+    
+    matriz2=(int**)malloc(sizeof(int*)*renglones2);
+    
+    for (i=1; i<=renglones2;i++){
+        matriz2[i]=(int*)malloc(sizeof(int)*columnas2);
+    }
 
-	// Reserva el tamaño en bytes
-	matriz = (int **) malloc(sizeof(double*)*renglones);
-
-	for (i= 0; i<renglones;i++) {
-		matriz[i]= (double*)malloc(sizeof(double)*columnas);
-                
-                }
-
-	/*printf("El arreglo es: ");
-	for (contador = 0; contador < renglones; contador++) {
-		matriz[contador] = contador + 1;
-                
-                for(contador2=0; contador < columnas; contador2++){
-                    matriz[contador2]=contador+1;
-                }
-	}
-	printf("\n");
-        */
-	free(matriz);
-	return 0;
+        for(i=1;i<=renglones2;i++){
+            for(ii=1;ii<=columnas2;ii++){
+                 printf("Introducir valor de [%d][%d]: ",i,ii);
+                scanf("%d",&matriz2[i][ii]);
+            }
+        }
+    
+    free(matriz);
+    free(matriz2);
+      
+    return (0);
 }
