c4667efa71850c49c611b2cf4b845acaead51e5c
Tender Ticante <tenderticante@gmail.com>
2015-11-03 23:05:56 -0600

    Correccion proceso de matriz

--- matrices/nxn.c
+++ matrices/nxn.c
@@ -7,11 +7,17 @@ int main() {
     int **matriz;
     int i;
     int ii;
+    int j;
+    int k;
     int **matriz2;
     int renglones2;
     int columnas2;
+    int **matrizr;
+    int renglonesr;
+    int columnasr;
     
     //Primera Matriz
+    printf("Introduzca la primera matriz a multiplicar:\n ");
     printf("Numero de renglones: ");
     scanf("%d", &renglones);
     
@@ -32,6 +38,7 @@ int main() {
         }
     
     //Segunda Matriz
+    printf("Introduzca la segunda matriz a multiplicar:\n ");
     printf("Numero de renglones: ");
     scanf("%d", &renglones2);
     
@@ -50,9 +57,37 @@ int main() {
                 scanf("%d",&matriz2[i][ii]);
             }
         }
-    
+    if(renglones!=columnas2){
+        printf(" \n Operacion Imposible \n\n El numero de renglones es diferente al numero de columnas de la segunda matriz!!! \n");
+        return(1);
+        }
+    else{
+       
+	 //Matriz resultado
+        
+        columnasr=columnas2;
+        renglonesr=renglones;
+        
+        matrizr=(int**)malloc(sizeof(int*)*renglonesr);
+        
+        for(i=1; i<=renglonesr;i++){
+            matrizr[i]=(int*)malloc(sizeof(int)*columnasr);
+        }
+        
+        //Operacion de matrices
+        for(i=1;i<=columnasr;i++){
+            for(ii=1;ii<=renglonesr;ii++){
+                for(j=1;j<=columnasr;j++){
+                    matrizr[i][k]= 1;((matriz[i][k])*(matriz2[i][ii]))+((matriz[i][k])*(matriz2[k][ii]))+((matriz[i][k])*(matriz2[k][ii]));
+                    printf("Matriz Resultado %d %d" ,matrizr[i][k]);
+                }   
+            }
+        }
+    }
+        
     free(matriz);
     free(matriz2);
+    free(matrizr);
       
     return (0);
 }
