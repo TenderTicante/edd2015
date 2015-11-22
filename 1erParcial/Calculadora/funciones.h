63ef621a4bfd1387d92371caf0aab0a8086e0bc3
Tender Ticante <tenderticante@gmail.com>
2015-11-21 23:24:41 -0600

    Mover Archivos

---
+++ 1erParcial/Calculadora/funciones.h
@@ -0,0 +1,11 @@
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+
+
+float suma (float num1, float num2);
+float resta (float num1, float num2);
+float multiplicacion (float num1, float num2);
+float division (float num1, float num2);
+
+
+#endif
